6f6cfb50ffcb5896af2b0d116c6e657a1ff437cd
Joel Joseph <59699500+nerdylabs@users.noreply.github.com>
2020-12-19 14:57:53 +0530

    Add files via upload

---
+++ LinearRegression.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class LinearRegression():
+    def __init__(self):
+        self.J_history = []
+
+    def computecost(self,X, y):
+        J = 0
+        m = max(y.shape)
+        #X = np.concatenate((np.ones((m,1)), X), axis=1)
+        prediction = np.dot(X,self.theta)
+        sq_error = (prediction - y)**2
+        J = 1/(2*m) * np.sum(sq_error)
+        return J
+
+    def fit(self,X, y, alpha=0.01, iterations=1500, return_prams=False):
+        m = max(y.shape)
+        X = np.concatenate((np.ones((m,1)), X), axis=1)
+        n = min(X.shape)
+        self.theta = np.zeros((n,1))
+        #J_history = []
+        for i in range(0, iterations):
+            error = np.dot(X, self.theta) - y
+            self.theta = self.theta - ((alpha/m) * np.dot(X.T, error))
+            J = self.computecost(X, y)
+            self.J_history.append(J)
+
+        self.J_history = np.asarray(self.J_history)
+        self.J_history = self.J_history.reshape(iterations, 1)
+        #print(self.J_history.shape)
+        if return_prams ==True:
+            return self.J_history, self.theta
+
+
+    def plotdata(self, X, y,title="Title", X_label="X-axis", y_label="Y-axis"):
+        plt.scatter(X, y, color = 'red')
+        plt.title(title)
+        plt.xlabel(X_label)
+        plt.ylabel(y_label)
+        plt.show()
+
+    def plotregressor(self, X, y,title="Title", X_label="X-axis", y_label="Y-axis"):
+        m = max(y.shape)
+        x = X
+        X = np.concatenate((np.ones((m,1)), X), axis=1)
+        plt.scatter(x, y, color = 'red')
+        plt.plot(x, np.dot(X, self.theta), color = 'blue')
+        plt.title(title)
+        plt.xlabel(X_label)
+        plt.ylabel(y_label)
+        plt.show()
+
+    def plotcost(self, X_label="iterations", y_label="Cost value"):
+        random = np.asarray(list(range(0, max(self.J_history.shape))))
+        plt.plot(random, self.J_history, color = 'blue')
+        plt.title("cost function")
+        plt.xlabel(X_label)
+        plt.ylabel(y_label)
+        plt.show()
+
+
---
+++ try_linear_regression.py
@@ -0,0 +1,28 @@
+from LinearRegression import LinearRegression
+import numpy as np
+
+
+x= []
+y = []
+with open("ex1data1.txt", mode='r') as file:
+    for idx, lines in enumerate(file):
+        #print(idx, lines)
+        line = lines.split(",")
+        #print(line)
+        x.append(float(line[0]))
+        y.append(float(line[1]))
+
+
+x = np.asarray(x)
+y = np.asarray(y)
+x = x.reshape(x.shape[0], 1)
+y = y.reshape(y.shape[0], 1)
+print(x.shape)
+print(y.shape)
+
+
+model = LinearRegression.LinearRegression()
+model.fit(x, y)
+model.plotdata(x, y)
+model.plotregressor(x, y)
+model.plotcost(x, y)
